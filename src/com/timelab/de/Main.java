399b6dd94437804951224d4bf582ac7b2d706d45
jOkAr987 <johannes180599@gmail.com>
2018-11-12 15:54:06 +0100

    Neue Klasse Berechnung

--- src/com/timelab/de/Main.java
+++ src/com/timelab/de/Main.java
@@ -2,13 +2,18 @@ package com.timelab.de;
 
 public class Main {
 
-    public static void test(){
-        System.out.println("flasche");
-        System.out.println("buch");
-    }
 
     public static void main(String[] args) {
-        System.out.println("Pathfinder tool");
+        int[][] meinArray = new int[9][9];
+        for(int i=0; i<meinArray.length;i++){
+            for(int j = 0; j<meinArray.length;j++){
+                meinArray[i][j] = 0;
+            }
+        }
+        meinArray[1][1] = 1;
+        meinArray[5][5] = 2;
         new Window();
+        Berechnung berechnung = new Berechnung();
+        berechnung.methode1(meinArray);
     }
 }
