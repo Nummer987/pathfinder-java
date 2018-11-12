6cec4b8b75016746fb062755d986a5bd292e0430
Tim <jansent16@gmail.com>
2018-11-12 01:06:00 +0100

    added window class

---
+++ src/com/timelab/de/Window.java
@@ -0,0 +1,12 @@
+package com.timelab.de;
+
+import javax.swing.*;
+
+public class Window extends JFrame {
+
+    public Window() {
+        setLayout(null);
+        setVisible(true);
+        setSize(400,300);
+    }
+}
