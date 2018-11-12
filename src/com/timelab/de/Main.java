16479be909a10a86920262b531c60e4f04638f70
Tim <jansent16@gmail.com>
2018-11-12 17:17:35 +0100

    main update

--- src/com/timelab/de/Main.java
+++ src/com/timelab/de/Main.java
@@ -4,19 +4,6 @@ public class Main {
 
 
     public static void main(String[] args) {
-        int[][] meinArray = new int[9][9];
-        for(int i=0; i<meinArray.length;i++){
-            for(int j = 0; j<meinArray.length;j++){
-                meinArray[i][j] = 0;
-            }
-        }
-        meinArray[1][1] = 1;
-        meinArray[8][6] = 2;
-        meinArray[5][3] = 3;
         new Window();
-        Berechnung berechnung = new Berechnung();
-        berechnung.arrayAusgeben(meinArray);
-        berechnung.methode1(meinArray);
-        berechnung.arrayAusgeben(meinArray);
     }
 }
