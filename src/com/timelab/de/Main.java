c0c1e31968de44ed1fb70a7fef63831c513342a5
Tim <jansent16@gmail.com>
2018-11-12 01:10:48 +0100

    3

--- src/com/timelab/de/Main.java
+++ src/com/timelab/de/Main.java
@@ -3,7 +3,7 @@ package com.timelab.de;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Pathfinder tool2");
+        System.out.println("Pathfinder tool3");
         new Window();
 
     }
