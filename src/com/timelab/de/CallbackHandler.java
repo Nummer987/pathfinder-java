847943c87907443653f441dd3013c159808ffef2
Tim <jansent16@gmail.com>
2018-11-12 15:07:40 +0100

    1

--- src/com/timelab/de/CallbackHandler.java
+++ src/com/timelab/de/CallbackHandler.java
@@ -1,4 +1,11 @@
 package com.timelab.de;
 
-public class CallbackHandler {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CallbackHandler implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
